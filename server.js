007d4e22718c621cf9dd813c1c31c006759f4a96
Zer0cheros <christian.wiss@optimaedu.fi>
2022-01-31 10:34:58 +0200

    test

--- server.js
+++ server.js
@@ -2,4 +2,7 @@ const express = require("express");
 const server = express();
 const mongoose = require("mongoose");
 server.use(express.static("public"));
-server.use(express.urlencoded({extended:true}));
+server.use(express.urlencoded({extended:true}));      
+
+
+server.listen(3000)
